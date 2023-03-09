551ff7c40f838597fbef01b2d47c7e8fc63575f5
Santosh H M <santhm18@in.ibm.com>
2023-03-09 21:46:22 +0530

    changes updated
    changes updated

--- operantsystems/index.js
+++ operantsystems/index.js
@@ -231,8 +231,8 @@ indexApp.controller("indexController", function($scope, $http, $window) {
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    // slides[slideIndex-1].style.display = "block";
+    // dots[slideIndex-1].className += " active";
 
   }
 
